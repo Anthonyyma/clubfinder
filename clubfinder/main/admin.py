3dbfcaf8bd0261e5907d7f8c98300941f5c1bc7b
Pavan Khiani <pavan.khiani@gmail.com>
2021-08-10 15:31:29 -0700

    Added map functionality and initial models

--- clubfinder/main/admin.py
+++ clubfinder/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Business)
+admin.site.register(PublicSpeaking)
+admin.site.register(Sports)
+admin.site.register(STEM)
+admin.site.register(SummerCamps)
+admin.site.register(Tutoring)
+admin.site.register(YouthEmployment)
--- clubfinder/main/views.py
+++ clubfinder/main/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 import requests, json
 import os
+from geopy.geocoders import Nominatim
+from .models import *
 
 def index(response):
+    activity = ''
+    if response.method == "POST":
+        activity = response.POST.getlist('activityResult')[0]
+        if activity == 'Business / Finance':
+
+            q = Business.objects.all()
+            # q = Business.objects.filter(name='JA Southern Alberta')[0].name
+            print(q)
+
+
     return render(response, 'main/index.html', {})
+
+def map(response):
+    return render(response, 'main/map.html', {})
