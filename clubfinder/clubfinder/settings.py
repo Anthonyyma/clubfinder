dc556512777300e8d116656b0c4e6cb490b130ca
Pavan Khiani <pavan.khiani@gmail.com>
2021-08-12 13:33:31 -0700

    Connected search button to results and added map functionality

--- clubfinder/clubfinder/settings.py
+++ clubfinder/clubfinder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0po(26)@l=hhu*ofw9pxhpts(=cqf-$@beqpbjsle-u6_#nv$#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- clubfinder/main/views.py
+++ clubfinder/main/views.py
@@ -5,18 +5,96 @@ import os
 from geopy.geocoders import Nominatim
 from .models import *
 
+activity = ''
+
 def index(response):
-    activity = ''
+    names = []
+    descriptions = []
+    addresses = []
+    lists = []
+    global activity
     if response.method == "POST":
         activity = response.POST.getlist('activityResult')[0]
         if activity == 'Business / Finance':
+            for i in range(Business.objects.count()):
+                names.append(Business.objects.all()[i].name)
+                descriptions.append(Business.objects.all()[i].description)
+                addresses.append(Business.objects.all()[i].address)
+        
+        if activity == 'Public Speaking':
+            for i in range(PublicSpeaking.objects.count()):
+                names.append(PublicSpeaking.objects.all()[i].name)
+                descriptions.append(PublicSpeaking.objects.all()[i].description)
+                addresses.append(PublicSpeaking.objects.all()[i].address)
 
-            business_list = list(Business.objects.all())
-            # q = Business.objects.filter(name='JA Southern Alberta')[0].name
-            print(business_list)
+        if activity == 'Sports':
+            for i in range(Sports.objects.count()):
+                names.append(Sports.objects.all()[i].name)
+                descriptions.append(Sports.objects.all()[i].description)
+                addresses.append(Sports.objects.all()[i].address)
 
+        if activity == 'STEM':
+            for i in range(STEM.objects.count()):
+                names.append(STEM.objects.all()[i].name)
+                descriptions.append(STEM.objects.all()[i].description)
+                addresses.append(STEM.objects.all()[i].address)
+        
+        if activity == 'Summer Camps and Programs':
+            for i in range(SummerCamps.objects.count()):
+                names.append(SummerCamps.objects.all()[i].name)
+                descriptions.append(SummerCamps.objects.all()[i].description)
+                addresses.append(SummerCamps.objects.all()[i].address)
+        
+        if activity == 'Tutoring':
+            for i in range(Tutoring.objects.count()):
+                names.append(Tutoring.objects.all()[i].name)
+                descriptions.append(Tutoring.objects.all()[i].description)
+                addresses.append(Tutoring.objects.all()[i].address)
 
-    return render(response, 'main/index.html', {})
+        if activity == 'Youth Employment/Volunteering':
+            for i in range(YouthEmployment.objects.count()):
+                names.append(YouthEmployment.objects.all()[i].name)
+                descriptions.append(YouthEmployment.objects.all()[i].description)
+                addresses.append(YouthEmployment.objects.all()[i].address)
+        
+        lists = zip(names, descriptions, addresses)
+
+    return render(response, 'main/index.html', {'lists':lists})
 
 def map(response):
-    return render(response, 'main/map.html', {})
+    adr = ''
+    name = response.GET.get('name')
+
+    if activity == 'Business / Finance':
+        desc = Business.objects.filter(name=name)[0].description
+        adr = Business.objects.filter(name=name)[0].address
+    if activity == 'Public Speaking':
+        desc = PublicSpeaking.objects.filter(name=name)[0].description
+        adr = PublicSpeaking.objects.filter(name=name)[0].address
+    if activity == 'Sports':
+        desc = Sports.objects.filter(name=name)[0].description
+        adr = Sports.objects.filter(name=name)[0].address
+    if activity == 'STEM':
+        desc = STEM.objects.filter(name=name)[0].description
+        adr = STEM.objects.filter(name=name)[0].address
+    if activity == 'Summer Camps and Programs':
+        desc = SummerCamps.objects.filter(name=name)[0].description
+        adr = SummerCamps.objects.filter(name=name)[0].address
+    if activity == 'Tutoring':
+        desc = Tutoring.objects.filter(name=name)[0].description
+        adr = Tutoring.objects.filter(name=name)[0].address
+    if activity == 'Youth Employment/Volunteering':
+        desc = YouthEmployment.objects.filter(name=name)[0].description
+        adr = YouthEmployment.objects.filter(name=name)[0].address
+
+
+    geolocator = Nominatim(user_agent="convert_to_coords")
+
+    print(adr)
+
+    location = geolocator.geocode(adr)
+    # print(adr)
+    # print(location)
+    # print(location.latitude)
+    # print(location.longitude)
+    return render(response, 'main/map.html', {'name':name, 'desc':desc, 'adr':adr, 'lat':location.latitude, 'lng':location.longitude})
